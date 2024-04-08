a873baea367e5cd7f20e41bc991cf908d4bae662
RasuwanK <kalharaweragala@gmail.com>
2024-04-08 09:33:11 +0530

    Mapping table for followers

---
+++ models/maps/follow.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Table , Column, String, ForeignKey, DateTime
+from app.database import Base
+
+# Mapping table to map 2 users 
+follow_table = Table(
+	"friends",
+	Base.metadata,
+	Column("username",String, ForeignKey("user.username"), primary_key=True),
+	Column("following_username",String, ForeignKey("user.username") ,primary_key=True),
+	Column("date_followed",DateTime)
+)
