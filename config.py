aec9cc95d608f9ef876d4b83770aca6baab4ba07
RasuwanK <kalharaweragala@gmail.com>
2024-04-10 10:10:21 +0530

    Containing all configs on a single repo

---
+++ config.py
@@ -0,0 +1,10 @@
+# This contains all the backend configuration
+
+import os
+
+# Using dotenv for loading envs
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
