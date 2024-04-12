97ddabbcb577535249d8833752f6604b6e39911d
RasuwanK <kalharaweragala@gmail.com>
2024-04-12 11:35:03 +0530

    Ignoring a type errror

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 from config import DATABASE_URL
 
 engine = create_engine(
-	DATABASE_URL,
+	str(DATABASE_URL),
 	connect_args={"check_same_thread": False}
 )
 
