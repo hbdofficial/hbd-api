533d959a81f08bd6cfbb17f4ef2a926d4ab98162
RasuwanK <kalharaweragala@gmail.com>
2024-04-12 11:35:21 +0530

    Added user path operation

--- main.py
+++ main.py
@@ -1,27 +1,54 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import List
-from datetime import date
 
-# Model used to represent card info
+app = FastAPI()
+
+# This file contains everything related to the user route
+
+from pydantic import BaseModel
+from datetime import date, datetime
+
+from app.main import app 
+
+# Schema representing account data
+class Account(BaseModel):
+	name: str
+	email: str
+	birthday: date
+	gender: str
+
+# Schema representing credit card information
 class Card(BaseModel):
 	card_no: str
 	holder_name: str
 	expiry_date: date
 
-class Notification():
-	pass
+# Schema representing notification information
+class Notification(BaseModel):
+	timestamp: datetime
+	content: str
 
-# Model used to represent user model
-# TODO: Include stories
+# Schema representing user data response
 class User(BaseModel):
-	username: str
-	name: str
-	birthday: date # Will be converted to necessary date object
-	gender: str
-	email: str
-	profile_pic: str | None = None
-	friends: List['User']
-	card: List['Card']
+	account: Account
+	age: int
+	billing_details: list[Card]
+	notifications: list[Notification] | None
 
-app = FastAPI()
+@app.get("/users/{username}", response_model=User)
+def route(username: str) -> User:
+	return User(
+		account=Account(
+			name="Rasuwan Kalhara", 
+			email="kalharaweragala@gmail.com", 
+			birthday=date(2024,10,1), 
+			gender="male"
+		),
+		billing_details=[
+			Card(
+				holder_name="W.D.Y.R.Kalhara",
+				expiry_date=date(2027,7,2),
+				card_no="122342-34-3455-34"
+				)],
+			age=21,
+			notifications=None
+		)
