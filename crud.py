d685e18df4b617a56e104df6dc8fbecb76893c0d
RasuwanK <kalharaweragala@gmail.com>
2024-04-25 10:13:36 +0530

    Added a test download crud

--- crud.py
+++ crud.py
@@ -3,7 +3,16 @@ from sqlalchemy.orm import Session
 from sqlalchemy import select
 from app.models.user import User
 
-def get_user(db: Session, username: str):
+from schemas import UnsafeUser
+from firebase import storage
+
+# This function will return a Pydantic model corresponding to the return object
+def get_user(db: Session, username: str) -> UnsafeUser:
+	""" Retrieving a single user object """
 	query = select(User).where(User.username == username)
-	print(query)
+	# Returing the first user object
 	return db.scalars(query).first()
+
+def download():
+	return storage.child("profile_pics/pic.jpg").get_url("profile_pic.jpg")
+	
